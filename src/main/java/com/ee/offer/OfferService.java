55a3f5a1b09547806fea60ba6f24cec327c84aac
Narendra Pathai <narendra.pathai@gmail.com>
2021-09-29 11:21:05 -0400

    Removed FIXME

--- src/main/java/com/ee/offer/OfferService.java
+++ src/main/java/com/ee/offer/OfferService.java
@@ -7,6 +7,5 @@ import java.util.Optional;
 
 public interface OfferService {
     Optional<Discount> applyProductOffer(Product product, int quantity);
-    // FIXME we are passing the whole shopping cart which is bad
     Optional<Discount> applyCartOffer(ShoppingCartDto shoppingCart);
 }
